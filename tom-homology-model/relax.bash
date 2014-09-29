78946d285f1b529ed969aecc9f69d53a6a45c8ef
Alex Carlin <carlin@ucdavis.edu>
2014-09-29 12:21:09 -0700

    Added reaction scheme (note that text is now outdated)

--- tom-homology-model/relax.bash
+++
@@ -1,8 +0,0 @@
-#$ -N tmo-homo-relax
-#$ -cwd
-#$ -S /bin/bash
-##$ -t 1
-
-export PATH=/home/carlin/Rosetta/main/source/bin:$PATH
-export ROSETTA3_DB=/home/carlin/Rosetta/main/database
-rosetta_scripts.linuxgccrelease @relax-flags -s S_0133.pdb
