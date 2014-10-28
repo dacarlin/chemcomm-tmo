be37dea22e593057c77f70b0de43f6aebf8f81e2
Alex Carlin <carlin@ucdavis.edu>
2014-10-27 17:14:16 -0700

    Updated based on suggestions from Justin

--- figures/time-course.r
+++ figures/time-course.r
@@ -5,11 +5,13 @@ d$eth = d$eth * 0.7
 d$oxi = d$oxi * 0.7
 
 ggplot(d, group=sample, color=sample) +
-geom_boxplot(aes(d$mins, d$eth, d$oxi, group=mins, color=sample), width=0) + 
-#geom_boxplot(aes(d$mins, d$oxi, group=mins, color=sample), width=0) + 
-stat_smooth(aes(d$mins+1, d$eth, color="Ethene"), method=lm, formula=y~log(x,10), se=F) + 
+geom_point(aes(d$mins, d$oxi)) +
+#geom_boxplot(aes(d$mins, d$oxi, group=mins)) + 
+geom_point(mapping=aes(x=d$mins, y=d$eth), shape=2) +
+#geom_boxplot(aes(d$mins, d$oxi, group=mins)) + 
+stat_smooth(aes(d$mins+1, d$eth), method=lm, formula=y~log(x,10), se=F) + 
 stat_smooth(aes(d$mins+1, d$oxi, color="Oxirane"), method=lm, formula=y~log(x,10), se=F) +
-labs(x="Minutes", y="Micromolar", title="TOM A113F" ) + 
+labs(x="Minutes", y="Concentration (micromolar)", title="TOM A113F" ) + 
 theme_bw() +
 theme(legend.position = "none") + 
 ggsave('time-course.png',width=5,height=5) 
--- figures/variants.r
+++ figures/variants.r
@@ -1,13 +1,25 @@
 #r script for graphing TMO variants
 d=read.csv('variants.csv')
-d$Percent = d$Eth / max(d$Eth) * 100
-d$eth.umol =  d$Eth / max(d$Eth)
-d$Strain2 = reorder(d$Strain, -d$Eth)
+d$Percent = d$Eth / 16 
+d$Strain2 = factor(d$Strain, levels=c(
+  "Buffer",
+  "Wild type TOM",
+  "V106A",
+  "V106P+A113G",
+  "V106R+A113W",
+  "V106I+A113S",
+  "V106Q+A113Q",
+  "A113H",
+  "V106H+A113S",
+  "A113V",
+  "V106E",
+  "V106F",
+  "A113F"
+))
 
 library(ggplot2)
-ggplot(d, aes(Strain,Percent)) + 
-# tufte-style
-geom_boxplot(width=0.618, ) + coord_flip() + 
-labs(title="",x="", y="Percent ethene after 24 hours") + 
+ggplot(d, aes(Strain2,Percent)) + 
+geom_boxplot(width=0.618) + coord_flip() + 
+labs(title="",x="", y="Percent ethylene after 24 hours") + 
 theme_bw() + 
 ggsave('variants.png',width=5,height=5)
